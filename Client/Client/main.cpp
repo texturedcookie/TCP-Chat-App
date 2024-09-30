06a51421aa65790b8320f0462235db812821391d
texturedcookie <78194195+texturedcookie@users.noreply.github.com>
2024-09-29 21:31:19 -0500

    Client code

---
+++ Client/Client/main.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <wS2tcpip.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <thread>
+
+
+using namespace std;
+
+#pragma comment(lib,"ws2_32.lib")
+
+bool Initialize()
+{
+	WSADATA data;
+	return WSAStartup(MAKEWORD(2, 2), &data) == 0;
+}
+void SendMsg(SOCKET s)
+{
+	cout << "Enter your chat name: " << endl;
+	string name;
+	getline(cin, name);
+	string message;
+	while (1)
+	{
+		getline(cin, message);
+		string msg = name + " : " + message;
+		int bytesent = send(s, msg.c_str(), msg.length(), 0);
+		if (bytesent == SOCKET_ERROR)
+		{
+			cout << "error sending message" << endl;
+			break;
+		}
+		if (message == "quit")
+		{
+			cout << "stopping the application" << endl;
+			break;
+		}
+	}
+	closesocket(s);
+	WSACleanup();
+}
+
+void ReceiveMsg(SOCKET s)
+{
+	char buffer[4096];
+	int recvlength;
+	string msg = "";
+	while (1)
+	{
+		recvlength = recv(s, buffer, sizeof(buffer), 0);
+		if (recvlength <= 0)
+		{
+			cout << "disconnected from the server" << endl;
+			break;
+		}
+		else
+		{
+			msg = string(buffer, recvlength);
+			cout << msg << endl;
+		}
+	}
+	closesocket(s);
+	WSACleanup();
+}
+
+int main()
+{
+	// initialize winsock
+	if (!Initialize())
+	{
+		cout << "Initialize winsock failed" << endl;
+
+		return 1;
+	}
+
+
+	//create socket
+	SOCKET s;
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	if (s == INVALID_SOCKET)
+	{
+		cout << "Invalid socket created" << endl;
+		return 1;
+	}
+
+	int port = 12345;
+	string serveraddress = "127.0.0.1";
+	sockaddr_in serveraddr;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(port);
+
+	//bind
+	inet_pton(AF_INET, serveraddress.c_str(), &(serveraddr.sin_addr));
+
+	//connect socket to the server
+	if (connect(s, reinterpret_cast<sockaddr*>(&serveraddr), sizeof(serveraddr)) == SOCKET_ERROR)
+	{
+		cout << "not able to connect from server" << endl;
+		closesocket(s);
+		WSACleanup();
+		return 1;
+	}
+
+	cout << "Successfully connected to server" << endl;
+	thread senderthread(SendMsg, s);
+	thread receiverthread(ReceiveMsg, s);
+
+	senderthread.join();
+	receiverthread.join();
+
+
+	//send and recieve
+	/*string message = "hello world";
+	int bytesent = send(s, message.c_str(), message.length(), 0);
+	if (bytesent == SOCKET_ERROR)
+	{
+		cout << "Send failed" << endl;
+	}
+	*/
+
+	// close the socket
+
+	return 0;
+}
--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -3,7 +3,8 @@
 #include <wS2tcpip.h>
 #include <tchar.h>
 #include <stdio.h>
-
+#include <thread>
+#include <vector>
 
 
 using namespace std;
@@ -20,6 +21,40 @@ bool Initialize()
 	return WSAStartup(MAKEWORD(2, 2), &data) == 0;
 }
 
+void InteractWithClient(SOCKET clientSocket, vector<SOCKET>& clients)
+{
+	//Send and receive to interact with multiple clients 
+		cout << "Client connected" << endl;
+		char buffer[4096];
+
+		while (1)
+		{
+			int bytesrecvd = recv(clientSocket, buffer, sizeof(buffer), 0);
+
+			if (bytesrecvd <= 0)
+			{
+				cout << "Client disconnected" << endl;
+				break;
+			}
+			string message(buffer, bytesrecvd);
+			cout << "message from client : " << message << endl;
+
+			for (auto client : clients)
+			{
+				if (client != clientSocket)
+				{
+					send(client, message.c_str(), message.length(), 0);
+				}
+			}
+		}
+		auto it = find(clients.begin(), clients.end(), clientSocket);
+		if (it != clients.end())
+		{
+			clients.erase(it);
+		}
+	//close the socket
+	closesocket(clientSocket);
+}
 
 
 int main()
@@ -29,81 +64,78 @@ int main()
 		cout << "Could not initialize Winsock 2" << endl;
 		return 1;
 	}
-	//else if (Initialize() == 0)
-	//{
-//create the socket
-	cout << "server program" << endl;
-	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-	if (listenSocket == INVALID_SOCKET)
+	else
 	{
-		cout << "socket creation failed" << endl;
-		return 1;
-	}
+		//create the socket
+		cout << "server program" << endl;
+		SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-	//get ip and port it well run on local host 12345
+		if (listenSocket == INVALID_SOCKET)
+		{
+			cout << "socket creation failed" << endl;
+			return 1;
+		}
 
-	//create address structure
-	int port = 12345;
-	sockaddr_in serveraddr;
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_port = htons(port); //host to network short
+		//get ip and port it well run on local host 12345
 
-	//convert the ip address put it inside the sin_family in binary
-	if (InetPton(AF_INET, _T("0.0.0.0"), &serveraddr.sin_addr) != 1)
-	{
-		cout << "setting address structure failed" << endl;
+		//create address structure
+		int port = 12345;
+		sockaddr_in serveraddr;
+		serveraddr.sin_family = AF_INET;
+		serveraddr.sin_port = htons(port); //host to network short
+
+		//convert the ip address put it inside the sin_family in binary
+		if (InetPton(AF_INET, _T("0.0.0.0"), &serveraddr.sin_addr) != 1)
+		{
+			cout << "setting address structure failed" << endl;
+			closesocket(listenSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		//Bind the ip and port with the socket created
+			//bind
+		if (bind(listenSocket, reinterpret_cast<sockaddr*>(&serveraddr), sizeof(serveraddr)) == SOCKET_ERROR)
+		{
+			cout << "Bind failed" << endl;
+			closesocket(listenSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		//listen on the socket
+			//listen
+		if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+		{
+			cout << "Listen failed" << endl;
+			closesocket(listenSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		cout << "Server has started listening on port: " << port << " ." << endl;
+		
+		vector<SOCKET> clients;
+
+		while (1)
+		{
+			//accept
+			SOCKET clientSocket = accept(listenSocket, nullptr, nullptr);
+			if (clientSocket == INVALID_SOCKET)
+			{
+				cout << "Invalid client socket" << endl;
+			}
+
+			clients.push_back(clientSocket);
+			thread t1(InteractWithClient, clientSocket, std::ref(clients));
+			t1.detach();
+		}
+		
 		closesocket(listenSocket);
+
+		//clean up winsock library
 		WSACleanup();
-		return 1;
+
+		return 0;
 	}
-
-
-//Bind the ip and port with the socket created
-	//bind
-	if (bind(listenSocket, reinterpret_cast<sockaddr*>(&serveraddr), sizeof(serveraddr)) == SOCKET_ERROR)
-	{
-		cout << "Bind failed" << endl;
-		closesocket(listenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-//listen on the socket
-	//listen
-	if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
-	{
-		cout << "Listen failed" << endl;
-		closesocket(listenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	cout << "Server has started listening on port: " << port << " ." << endl;
-
-//accept
-	SOCKET clientSocket = accept(listenSocket, nullptr, nullptr);
-	if (clientSocket == INVALID_SOCKET)
-	{
-		cout << "Invalid client socket" << endl;
-	}
-
-//recieve
-	char buffer[4096];
-	int bytesrecvd = recv(clientSocket, buffer, sizeof(buffer), 0);
-	
-	string message(buffer, bytesrecvd);
-	cout << "message from client : " << message << endl;
-
-//send
-
-//close the socket
-	closesocket(clientSocket);
-	closesocket(listenSocket);
-
-//clean up winsock library
-	WSACleanup();
-			//}
-
-	return 0;
 }
